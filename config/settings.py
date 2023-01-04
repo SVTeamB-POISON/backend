936d845963f8d6c401c0a49b0c7dd9653767a869
0sik <park-youngsik@bag-yeongsig-ui-MacBookAir.local>
2023-01-04 21:49:21 +0900

    fix : 데이터베이스 오류 수정

--- config/settings.py
+++ config/settings.py
@@ -86,11 +86,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'flower_flowerlist',
+        'HOST': '127.0.0.1',
+        'PORT': 27017,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
