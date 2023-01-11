4e1996a02cf6e79ea7af86b5d5ec1746fc5b4e8b
JunRain <sa2313@tukorea.ac.kr>
2023-01-11 15:32:19 +0900

    refact : upload -> image

--- flower/urls.py
+++ flower/urls.py
@@ -4,6 +4,6 @@ from .views import FlowerDecisionAPI, FlowerList, FlowerDetail
 
 urlpatterns = [
     path('', FlowerList.as_view()),
-    path('upload/', FlowerDecisionAPI.as_view()),
+    path('image/', FlowerDecisionAPI.as_view()),
     path('details/', FlowerDetail.as_view())
 ]
