2b557bbb9cd311172cd6298fd98ffa560a9a4e63
GiHwan2 <sandwich5011@naver.com>
2023-01-12 17:09:31 +0900

    feat : ranking 기능 구현 완료

--- flower/updater.py
+++ flower/updater.py
@@ -4,15 +4,14 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from .models import Flower
 def start():
     scheduler = BackgroundScheduler()
-    #scheduler.add_job(schedule_api,'cron',second=10)
-    scheduler.add_job(schedule_api,'cron',second=1)#minute
+    scheduler.add_job(schedule_api,'cron',minute=0)
     scheduler.start()
 
 
 
 def schedule_api():
     for i in Flower.objects.all():
-                i.count=0
-                i.save()
+        i.count=0
+        i.save()
 
             
