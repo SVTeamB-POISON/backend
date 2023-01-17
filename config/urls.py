50060d4651fdc2deef1ebdb95723af5711db191f
GiHwan2 <sandwich5011@naver.com>
2023-01-17 14:13:05 +0900

    fix : swagger 배포환경 https 오류 수정

--- config/urls.py
+++ config/urls.py
@@ -33,9 +33,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    re_path(r'api/swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'api/swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'api/redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
     path('api/admin/', admin.site.urls),
     path('api/flowers/', include('flower.urls')),
     path("api/",include("django_prometheus.urls"))
