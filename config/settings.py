e68850c21e6631190809a3d35306c4d2ae74bba6
GiHwan2 <sandwich5011@naver.com>
2023-01-25 16:29:08 +0900

    Revert "Merge pull request #101 from SVTeamB-POISON/fix/#100"
    This reverts commit 84b6d79ba119bb4c303373615ca8a553a3816518.

--- config/settings.py
+++ config/settings.py
@@ -149,4 +149,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
